19aa9858efc35c4cfe0b48fbf6d9927bca8c7063
Luka Aleksić <laleksic@mail.ru>
2021-07-08 13:01:10 +0200

    Initial commit

---
+++ rawinput_demo.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include "rawinput.h"
+
+#pragma GCC diagnostic ignored "-Wint-to-pointer-cast"
+#pragma GCC diagnostic ignored "-Wpointer-to-int-cast"
+
+/* Event handling callbacks */
+void sample_on_key_down(void *tag, Raw_Key key, void *_) {
+    printf("Device %d: Key %s was pressed.\n", (int)tag, raw_key_str(key));
+}
+
+void sample_on_key_up(void *tag, Raw_Key key, void *_) {
+    printf("Device %d: Key %s was released.\n", (int)tag, raw_key_str(key));
+}
+
+void sample_on_rel(void *tag, Raw_Axis axis, int delta, void *_) {
+    printf("Device %d: Movement %d on axis %s.\n", (int)tag, delta, raw_axis_str(axis));
+}
+
+void sample_on_plug(int idx, void *_) {
+    // Use a counter to assign a unique id to each device as it's plugged in
+    int *next_tag = (int*)_;
+    int tag = *next_tag;
+    *next_tag = tag + 1;
+
+    raw_open(idx, (void*)tag);
+    printf("Device %d at idx %d plugged.\n", (int)tag, idx);
+}
+
+void sample_on_unplug(void *tag, void *_) {
+    raw_close(tag);
+    printf("Device %d unplugged.\n", (int)tag);
+}
+/**/
+
+int main (int argc, char **argv) {
+    // Initialize library
+    raw_init();
+
+    // Count available devices
+    int dev_cnt = raw_dev_cnt();
+    printf("Detected %d devices.\n", dev_cnt);
+
+    #ifdef __linux__
+    if (dev_cnt == 0) {
+        /* Linux receives raw input events from all windows, however
+         * the price is that the user that runs the program has to do
+         * some system administration to make sure the /dev/input/event*
+         * files are readable. Usually the simplest way is to just run
+         * the program as root. */
+        puts("Did you make sure that /dev/input/event* files are readable by this user?");
+    }
+    #endif
+
+    // Open all available devices
+    int next_tag = 0;
+    for (int i=0; i<dev_cnt; ++i) {
+        raw_open(i, (void*)(next_tag++));
+    }
+
+    // Register event handlers
+    raw_on_key_down(sample_on_key_down, 0);
+    raw_on_key_up(sample_on_key_up, 0);
+    raw_on_rel(sample_on_rel, 0);
+    raw_on_unplug(sample_on_unplug, 0);
+    raw_on_plug(sample_on_plug, &next_tag);
+
+    #ifdef _WIN32
+    /* Windows requires us to have a window open to receive raw input events,
+     * and we only receive events from windows owned by our process.
+     *
+     * Usually you'd open a window anyway and use a cross-platform library
+     * like SDL, but here for simplicity we use win32 and only open a window
+     * when running on windows. */
+    WNDCLASS wc;
+    HINSTANCE instance = 0;
+    memset(&wc, 0, sizeof(wc));
+    wc.lpfnWndProc = DefWindowProc;
+    wc.hInstance = instance;
+    wc.lpszClassName = "sample_class";
+    RegisterClass(&wc);
+
+    HWND hwnd = CreateWindow("sample_class", "sample", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 0, 0, 0, 0, HWND_DESKTOP, NULL, instance, NULL);
+    #endif
+
+    puts("Entering event loop.");
+    #ifdef _WIN32
+    puts("Close the window to terminate.");
+    #else
+    puts("Press Control+C to terminate.");
+    #endif
+
+    #ifdef _WIN32
+    /* Here we handle the "windowing" events that rawinput doesn't handle;
+     * Again usually you'd use cross-platform SDL code here instead of win32 */
+    MSG msg;
+    while (GetMessage(&msg, hwnd, 0, 0) >= 0) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+
+        // Check for raw input and call event handlers
+        raw_poll();
+    }
+    #elif __linux__
+    for (;;) {
+        // Check for raw input and call event handlers
+        raw_poll();
+    }
+    #endif
+
+    // Clean up
+    raw_quit();
+}
---
+++ readme.md
@@ -0,0 +1,23 @@
+# librawinput
+
+## Description
+
+librawinput is a cross-platform (Linux/Windows) wrapper over low-level raw input capabilities. Raw input allows getting input from different keyboards/mice and telling the difference between them-- a feature lacking in input systems such as SDL's input system. A use case would be a split screen game allowing each player to use his own mouse/keyboard pair. 
+
+## Limitations
+
+- On Linux requires user to have read access to `/dev/input/*` files.
+- For now, only supports ANSI standard keyboard - full form factor and standard 3-button mice.
+- No documentation yet, though a sample program is included.
+
+## Compiling
+
+Compile your program with `rawinput_common.c` and `rawinput_linux.c` or `rawinput_win32.c` depending on your platform. `rawinput.h` contains the public API.
+
+The file `rawinput_demo.c` is an example program. You can compile it with:
+`gcc rawinput_common.c rawinput_linux.c rawinput_demo.c -o rawinput_demo` if you're using `gcc` on Linux, or an equivalent command-line for other platforms.
+
+## Usage
+
+No documentation is yet available. For all usage information, read the included demo program.
+
